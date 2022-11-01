66ae575a880b06e5fd6836a5b165e31304afc7b3
Mohammad Kazemiraz <kazemiraz72@gmail.com>
2022-11-01 10:54:33 +0330

    fix direction

--- src/pages/Home/Home.page.js
+++ src/pages/Home/Home.page.js
@@ -1,15 +1,15 @@
-import {Helmet} from "react-helmet";
-import {UserLayout} from "../../layouts";
+import { Helmet } from "react-helmet";
+import { UserLayout } from "../../layouts";
 
-const Home = ()=>{
-    return (
-        <>
-            <Helmet>
-                <title>صفحه اصلی</title>
-            </Helmet>
-            <UserLayout children={<h1>Home</h1>} />
-        </>
-    )
-}
+const Home = () => {
+  return (
+    <>
+      <Helmet>
+        <title>صفحه اصلی</title>
+      </Helmet>
+      <UserLayout children={<h1>Home</h1>} />
+    </>
+  );
+};
 
-export {Home};
+export { Home };
