6ed9e3756b8eae15045c1018a363bd43163d30b8
Mohammad Kazemiraz <kazemiraz72@gmail.com>
2022-11-02 16:53:25 +0330

    fix

--- src/api/announcement.api.js
+++ src/api/announcement.api.js
@@ -1,5 +1,5 @@
 import http from "../services/http.service";
-
+// TODO: question and announcement
 export async function getAnnouncements(url, config) {
   try {
     return await http.getAll(url, config);
