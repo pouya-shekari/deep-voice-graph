07c8d0af5b90d56923c139dd9c87d7bffc340886
Mohamad <kazemiraz72@gmail.com>
2022-11-08 17:46:53 +0330

    feature/checker-add-delete

---
+++ src/api/checker.api.js
@@ -0,0 +1,25 @@
+import http from "../services/http.service";
+// TODO: question and announcement
+export async function getCheckers(url, config) {
+  try {
+    return await http.getAll(url, config);
+  } catch (e) {
+    return e;
+  }
+}
+
+export async function addChecker(url, data, config) {
+  try {
+    return await http.post(url, data, config);
+  } catch (e) {
+    throw Error(e.response.status);
+  }
+}
+
+export async function deleteChecker(url, config) {
+  try {
+    return await http.delete(url, config);
+  } catch (e) {
+    throw Error(e.response.status);
+  }
+}
--- src/helpers/convertBreadcrumbsToPersian.helper.js
+++ src/helpers/convertBreadcrumbsToPersian.helper.js
@@ -1,5 +1,8 @@
 import CampaignIcon from "@mui/icons-material/Campaign";
 import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
+import SettingsInputComponentIcon from "@mui/icons-material/SettingsInputComponent";
+import CheckIcon from "@mui/icons-material/Check";
+import AccountTreeIcon from "@mui/icons-material/AccountTree";
 
 const convertBreadcrumbs = (value) => {
   switch (value) {
@@ -15,6 +18,27 @@ const convertBreadcrumbs = (value) => {
         <>
           <QuestionMarkIcon sx={{ mr: 0.5 }} fontSize="inherit" />
           مدیریت سوال‌ها
+        </>
+      );
+    case "actions":
+      return (
+        <>
+          <SettingsInputComponentIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+          مدیریت عملکردها
+        </>
+      );
+    case "checkers":
+      return (
+        <>
+          <CheckIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+          مدیریت چکرها
+        </>
+      );
+    case "flows":
+      return (
+        <>
+          <AccountTreeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+          مدیریت فلوچارت‌ها
         </>
       );
     default:
