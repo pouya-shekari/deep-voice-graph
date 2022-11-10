3b0b5125dd04cb49d6b2c82affd2b92d43fa5c49
Mohamad <kazemiraz72@gmail.com>
2022-11-10 15:14:13 +0330

    feature

--- src/components/Flow/DrawFlow/Chart/Chart.jsx
+++ src/components/Flow/DrawFlow/Chart/Chart.jsx
@@ -8,28 +8,10 @@ import ReactFlow, {
   Background,
   ReactFlowProvider,
   MiniMap,
-  useStore,
 } from "reactflow";
 import "reactflow/dist/style.css";
-
 const initialNodes = [];
-
 const initialEdges = [];
-const defaultEdgeOptions = {
-  style: { strokeWidth: 1, stroke: "black" },
-  type: "smoothstep",
-  labelBgPadding: [8, 4],
-  labelBgBorderRadius: 4,
-  labelBgStyle: { fill: "#fff", color: "#fff", fillOpacity: 1 },
-  labelStyle: { fontSize: 10, fontWeight: 700 },
-  markerEnd: {
-    type: MarkerType.Arrow,
-    color: "black",
-    width: 20,
-    height: 20,
-  },
-};
-const snapGrid = [20, 20];
 const Chart = () => {
   const reactFlowWrapper = useRef(null);
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
@@ -45,14 +27,10 @@ const Chart = () => {
       event.preventDefault();
 
       const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
-      console.log(reactFlowBounds);
-      console.log(event.clientX);
-      console.log(event.clientY);
       const type = event.dataTransfer.getData("application/reactflow");
       if (typeof type === "undefined" || !type) {
         return;
       }
-      console.log(reactFlowInstance.getZoom());
       const position = reactFlowInstance.project({
         x:
           event.clientX -
@@ -60,7 +38,6 @@ const Chart = () => {
           1650 * reactFlowInstance.getZoom(),
         y: event.clientY - reactFlowBounds.top,
       });
-      console.log(position);
       const newNode = {
         id: Math.random().toString(),
         type: "default",
@@ -83,11 +60,6 @@ const Chart = () => {
           onDragOver={dragOverHandler}
           onInit={setReactFlowInstance}
           connectionLineType={ConnectionLineType.SmoothStep}
-          panOnScroll={false}
-          defaultZoom={1.5}
-          snapToGrid={true}
-          snapGrid={snapGrid}
-          attributionPosition="top-right"
         >
           <Controls showFitView={false} />
           <Background />
