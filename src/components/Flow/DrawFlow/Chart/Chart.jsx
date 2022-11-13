c4ed5e210e0b82d50bfb277074c0041ebcae0fa4
Mohamad <kazemiraz72@gmail.com>
2022-11-13 10:45:00 +0330

    feature/user cant add resource to start and end

--- src/components/Flow/DrawFlow/Chart/Chart.jsx
+++ src/components/Flow/DrawFlow/Chart/Chart.jsx
@@ -12,6 +12,9 @@ import ReactFlow, {
 import { v4 as uuidv4 } from "uuid";
 import "reactflow/dist/style.css";
 
+import Snak from "../../../Snak/Snak";
+import Modal from "../../../UI/Modal/Modal";
+
 import getNodeTypes from "../../../../helpers/getNodeTypes";
 import GetDefaultEdgeOptions from "../../../../helpers/getDefaultEdgeOptions";
 import convertNodeNames from "../../../../helpers/convertNodeNames";
@@ -23,11 +26,18 @@ const Chart = () => {
   const [nodes, setNodes, onNodesChange] = useNodesState([]);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
   const [reactFlowInstance, setReactFlowInstance] = useState(null);
+  const [showResourceModal, setShowResourceModal] = useState(false);
+  const [snak, setSnak] = useState({
+    message: "",
+    type: "",
+    open: false,
+  });
 
   const dragOverHandler = useCallback((event) => {
     event.preventDefault();
     event.dataTransfer.dropEffect = "move";
   }, []);
+
   const dropHandler = useCallback(
     (event) => {
       event.preventDefault();
@@ -60,30 +70,77 @@ const Chart = () => {
     [setEdges, edges]
   );
 
+  const addResourceHandler = (event, node) => {
+    // Can use node type and update data, label, ...
+    console.log(node);
+    if (node.type === "Start" || node.type === "End") {
+      setSnak({
+        type: "error",
+        message: "افزودن Resource به نقاط شروع و پایان امکان‌پذیر نمی‌باشد.",
+        open: true,
+      });
+      return;
+    }
+    setShowResourceModal(true);
+  };
+
+  const closeModal = () => {
+    setShowResourceModal(false);
+  };
+
+  const handleSnakClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setSnak({ ...snak, open: false });
+  };
+
   return (
-    <div className="mt-3">
-      <div style={{ height: "100vh", direction: "ltr" }}>
-        <ReactFlowProvider>
-          <ReactFlow
-            nodeTypes={nodeTypes}
-            ref={reactFlowWrapper}
-            nodes={nodes}
-            edges={edges}
-            onNodesChange={onNodesChange}
-            defaultEdgeOptions={defaultEdgeOptions}
-            onDrop={dropHandler}
-            onDragOver={dragOverHandler}
-            onInit={setReactFlowInstance}
-            connectionLineType={ConnectionLineType.SmoothStep}
-            onConnect={onconnect}
-          >
-            <Controls />
-            <Background />
-            <MiniMap />
-          </ReactFlow>
-        </ReactFlowProvider>
+    <>
+      <Snak
+        isOpen={snak.open}
+        type={snak.type}
+        message={snak.message}
+        onClose={handleSnakClose}
+      />
+      <Modal
+        open={showResourceModal}
+        onClose={closeModal}
+        title={"افزودن Resource به Node"}
+        actions={[
+          { type: "add", label: "افزودن", onClick: () => {} },
+          { type: "cancel", label: "انصراف", onClick: closeModal },
+        ]}
+        description={
+          " برای افزودن Resource ابتدا آن را جستجو و سپس از لیست نمایش داده شده انتخاب نمایید."
+        }
+      ></Modal>
+      <div className="mt-3">
+        <div style={{ height: "100vh", direction: "ltr" }}>
+          <ReactFlowProvider>
+            <ReactFlow
+              nodeTypes={nodeTypes}
+              ref={reactFlowWrapper}
+              nodes={nodes}
+              edges={edges}
+              onNodesChange={onNodesChange}
+              defaultEdgeOptions={defaultEdgeOptions}
+              onDrop={dropHandler}
+              onDragOver={dragOverHandler}
+              onInit={setReactFlowInstance}
+              connectionLineType={ConnectionLineType.SmoothStep}
+              onConnect={onconnect}
+              deleteKeyCode="Delete"
+              onNodeDoubleClick={addResourceHandler}
+            >
+              <Controls />
+              <Background />
+              <MiniMap />
+            </ReactFlow>
+          </ReactFlowProvider>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
