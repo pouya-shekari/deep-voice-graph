fca07ee51c81bd4639818a79e60b44121e9fd3c8
Mohamad <kazemiraz72@gmail.com>
2022-11-20 15:17:17 +0330

    feature/forward node added

--- src/helpers/convertNodeNames.js
+++ src/helpers/convertNodeNames.js
@@ -6,6 +6,7 @@ const convertNodeNames = (name) => {
     Question: "سوال",
     Checker: "چکر",
     End: "پایان",
+    Forward: "ارجاع",
   };
   return mapName[name] || name;
 };
--- src/helpers/getNodeTypes.js
+++ src/helpers/getNodeTypes.js
@@ -2,6 +2,7 @@ import Action from "../components/Flow/DrawFlow/Node/CustomeNodes/Action/Action"
 import Announcement from "../components/Flow/DrawFlow/Node/CustomeNodes/Announcement/Announcement";
 import Checker from "../components/Flow/DrawFlow/Node/CustomeNodes/Checker/Checker";
 import End from "../components/Flow/DrawFlow/Node/CustomeNodes/End/End";
+import Forward from "../components/Flow/DrawFlow/Node/CustomeNodes/Forward/End";
 import Question from "../components/Flow/DrawFlow/Node/CustomeNodes/Question/Question";
 import Start from "../components/Flow/DrawFlow/Node/CustomeNodes/Start/Start";
 
@@ -13,6 +14,7 @@ const getNodeTypes = () => {
     Question: Question,
     Action: Action,
     End: End,
+    Forward: Forward,
   };
 };
 
