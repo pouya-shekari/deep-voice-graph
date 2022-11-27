d1683c52491eb3912a04f4ac84b2c5bdb91aa88e
Mohamad <kazemiraz72@gmail.com>
2022-11-27 12:02:21 +0330

    user can reset flow charts.

--- src/components/Flow/Chart/Reset/index.jsx
+++ src/components/Flow/Chart/Reset/index.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Button } from "@mui/material";
+import RestartAltIcon from "@mui/icons-material/RestartAlt";
+import styles from "./index.module.scss";
 
-const Reset = () => {
-  return <div></div>;
+const Reset = ({ onClick, isLoading }) => {
+  return (
+    <>
+      <Button
+        variant="contained"
+        color="warning"
+        startIcon={<RestartAltIcon className={isLoading && styles.reset} />}
+        onClick={onClick}
+        disabled={isLoading}
+      >
+        بازنشانی فلوچارت
+      </Button>
+    </>
+  );
 };
 
 export default Reset;
--- src/components/Flow/DragableNodes/index.jsx
+++ src/components/Flow/DragableNodes/index.jsx
@@ -5,7 +5,7 @@ import styles from "./index.module.scss";
 import Node from "@cmp/Flow/DragableNodes/Node";
 const DragableNodes = () => {
   return (
-    <Box className={styles.nodeWrapper} sx={{ mb: 3 }}>
+    <Box className={styles.nodeWrapper} sx={{ mb: 2 }}>
       {NODETYPES.map((node, index) => (
         <Node {...node} key={index} />
       ))}
